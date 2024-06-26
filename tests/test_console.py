9d29e509ad28065c9eaf3a983fdb576c1d7ed554
yawempire <manuelasantempire@gmail.com>
2024-06-26 03:00:15 +0000

    Tests for console

---
+++ tests/test_console.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Test for the console module
+"""
+import unittest
+from unittest.mock import patch
+from io import StringIO
+import console import HBNBCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """
+    Test for the entire console file
+    """
+
+    def setUp(self):
+        """
+        set up test class for all
+        """
+        self.held_output = StringIO()
+
+    def tearDown(self):
+        """
+        releases memory after every test
+        """
+        self.held_output.close()
+
+    def assert_stdout(self, expected_output, mock_stdout):
+        with patch('sys.stdout', new=self.held_output) as cmd_file:
+            HBNBCommand().onecmd(expect_output)
+            self.assertEqual(
+                    self.held_output.getvalue().strip(),
+                    expected_output
+                )
+
+    def test_quit(self):
+        """
+        Tests the quit function
+        """
+        self.assert_stdout('', 'quit\n')
+
+    def test_EOF(self):
+        """
+        Tests the EOF function
+        """
+        self.assert_stdout('', 'EOF\n')
+
+    def test_help(self):
+        """
+        Tests the help function
+        """
+        self.assert_stdout('', 'help\n')
+
+    def test_empty_line(self):
+        """
+        Tests when nothing is entered or an empty line encountered
+        """
+        self.assert_stdout('', '\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
